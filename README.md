8fb5d3e068b5a75a97c1d8b4dbd1fbd9c1a874dc
Brandon <brandon@diginow.it>
2021-02-18 15:49:00 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+McDonalds API
+====
+
+I am going to reverse engineer the mcdonalds api and make a public module to integrate with it.
+
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('preparing to make this api integration');
+
+const mcdonalds={};
+
+export {
+    mcdonalds as default,
+    mcdonalds
+}
